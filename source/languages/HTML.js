052f5d5f2279556747f28a80bf546230cc4ae75a
woXrooX <woXrooX@gmail.com>
2024-09-21 18:45:53 +0500

    Style change

--- source/languages/HTML.js
+++ source/languages/HTML.js
@@ -1,171 +1,166 @@
 export default class HTML{
-  static #tokens = [];
-  static #current = 0;
-  static #code = "";
+	static #tokens = [];
+	static #current = 0;
+	static #code = "";
 
-  static handle(code){
-    HTML.#reset();
-    HTML.#code = code;
-    HTML.#tokenize();
-    return HTML.#renderHighlightedCode();
-  }
+	static handle(code){
+		HTML.#reset();
+		HTML.#code = code;
+		HTML.#tokenize();
+		return HTML.#render_highlighted_code();
+	}
 
-  static #reset(){
-    HTML.#tokens = [];
-    HTML.#current = 0;
-    HTML.#code = "";
-  }
+	static #reset(){
+		HTML.#tokens = [];
+		HTML.#current = 0;
+		HTML.#code = "";
+	}
 
-  static #renderHighlightedCode(){
-    let html = '';
+	static #render_highlighted_code(){
+		let html = '';
 
-    for(const token of HTML.#tokens){
-      let tokenHtml = '';
+		for(const token of HTML.#tokens){
+			let tokenHtml = '';
 
-      switch(token.type){
-        case 'comment':
-          tokenHtml = `<span style="color: grey;">${token.value}</span>`;
-          break;
+			switch(token.type){
+				case 'comment':
+					tokenHtml = `<span style="color: grey;">${token.value}</span>`;
+					break;
 
-        case 'symbol':
-          tokenHtml = `<span style="color: white;">${token.value}</span>`;
-          break;
+				case 'symbol':
+					tokenHtml = `<span style="color: white;">${token.value}</span>`;
+					break;
 
-        case 'string':
-          tokenHtml = `<span style="color: hsla(124, 38%, 58%, 1);">${token.value}</span>`;
-          break;
+				case 'string':
+					tokenHtml = `<span style="color: hsla(124, 38%, 58%, 1);">${token.value}</span>`;
+					break;
 
-        case 'elementName':
-          tokenHtml = `<span style="color: red;">${token.value}</span>`;
-          break;
+				case 'elementName':
+					tokenHtml = `<span style="color: red;">${token.value}</span>`;
+					break;
 
-        case 'attribute':
-          tokenHtml = `<span style="color: orange;">${token.value}</span>`;
-          break;
+				case 'attribute':
+					tokenHtml = `<span style="color: orange;">${token.value}</span>`;
+					break;
 
-        case 'text':
-          tokenHtml = `<span style="color: white;">${token.value}</span>`;
-          break;
+				case 'text':
+					tokenHtml = `<span style="color: white;">${token.value}</span>`;
+					break;
 
-        case 'whitespace':
-          tokenHtml = `<span>${token.value}</span>`;
-          break;
+				case 'whitespace':
+					tokenHtml = `<span>${token.value}</span>`;
+					break;
 
-        case 'newline':
-          tokenHtml = `<span>${token.value}</span>`;
-          break;
+				case 'newline':
+					tokenHtml = `<span>${token.value}</span>`;
+					break;
 
-        default: tokenHtml = `<span style="color: grey;">${token.value}</span>`;
-      }
+				default: tokenHtml = `<span style="color: grey;">${token.value}</span>`;
+			}
 
-      html += tokenHtml;
-    }
+			html += tokenHtml;
+		}
 
-    return html;
-  }
+		return html;
+	}
 
-  ////// Tokenizer & Helpers
-  static #tokenize(){
-    Main: while(HTML.#current < HTML.#code.length){
-      // ' '
-      if(HTML.#code[HTML.#current] === ' '){
-        HTML.#handleWhitespace();
-      }
+	////// Tokenizer & Helpers
+	static #tokenize(){
+		Main: while(HTML.#current < HTML.#code.length){
+			// ' '
+			if(HTML.#code[HTML.#current] === ' ') HTML.#handle_whitespace();
 
-      // '\n'
-      if(HTML.#code[HTML.#current] === '\n'){
-        HTML.#handleNewLine();
-      }
+			// '\n'
+			if(HTML.#code[HTML.#current] === '\n') HTML.#handle_newLine();
 
-      // Element & attributes start
-      // '<'
-      if(HTML.#code.substring(HTML.#current, HTML.#current+4) === "&lt;"){
-        HTML.#handleElement();
-        continue;
-      }
+			// Element & attributes start
+			// '<'
+			if(HTML.#code.substring(HTML.#current, HTML.#current+4) === "&lt;"){
+				HTML.#handle_element();
+				continue;
+			}
 
-      if(HTML.#code[HTML.#current]){
-        HTML.#tokens.push({type: 'text', value: HTML.#code[HTML.#current]});
-        HTML.#current++;
-      }
-    }
-  }
+			if(HTML.#code[HTML.#current]){
+				HTML.#tokens.push({type: 'text', value: HTML.#code[HTML.#current]});
+				HTML.#current++;
+			}
+		}
+	}
 
-  static #handleComment(){
-    HTML.#current += 7;
+	static #handle_comment(){
+		HTML.#current += 7;
 
-    HTML.#tokens.push({type: 'comment', value: "&lt;-- "});
+		HTML.#tokens.push({type: 'comment', value: "&lt;-- "});
 
-    while(HTML.#code.substring(++HTML.#current, HTML.#current+4) !== "&gt;" && HTML.#current < HTML.#code.length)
-      HTML.#tokens.push({type: 'comment', value: HTML.#code[HTML.#current]});
+		while(HTML.#code.substring(++HTML.#current, HTML.#current+4) !== "&gt;" && HTML.#current < HTML.#code.length) HTML.#tokens.push({type: 'comment', value: HTML.#code[HTML.#current]});
 
-    HTML.#tokens.push({type: 'comment', value: "&gt;"});
+		HTML.#tokens.push({type: 'comment', value: "&gt;"});
 
-    HTML.#current += 4;
-  }
+		HTML.#current += 4;
+	}
 
-  static #handleWhitespace(){
-    HTML.#tokens.push({type: 'whitespace', value: ' '});
-    HTML.#current++;
-  }
+	static #handle_whitespace(){
+		HTML.#tokens.push({type: 'whitespace', value: ' '});
+		HTML.#current++;
+	}
 
-  static #handleNewLine(){
-    HTML.#tokens.push({type: 'newline', value: '\n'});
-    HTML.#current++;
-  }
+	static #handle_newLine(){
+		HTML.#tokens.push({type: 'newline', value: '\n'});
+		HTML.#current++;
+	}
 
-  static #handleElement(){
-    // Comment
-    if(HTML.#code.substring(HTML.#current+4, HTML.#current+7) === "!--"){
-      HTML.#handleComment();
-      return;
-    }
+	static #handle_element(){
+		// Comment
+		if(HTML.#code.substring(HTML.#current+4, HTML.#current+7) === "!--"){
+			HTML.#handle_comment();
+			return;
+		}
 
-    // Or normal opening
-    else HTML.#handleSymbols('<', 3);
+		// Or normal opening
+		else HTML.#handle_symbols('<', 3);
 
-    // Loop until receiving ">"
-    while(HTML.#code[++HTML.#current] !== "&gt;" && HTML.#current < HTML.#code.length){
-      // = /
-      if(
-        HTML.#code[HTML.#current] === '=' ||
-        HTML.#code[HTML.#current] === '/' ||
-        HTML.#code[HTML.#current] === '!'
-      ) HTML.#handleSymbols();
+		// Loop until receiving ">"
+		while(HTML.#code[++HTML.#current] !== "&gt;" && HTML.#current < HTML.#code.length){
+			// = /
+			if(
+				HTML.#code[HTML.#current] === '=' ||
+				HTML.#code[HTML.#current] === '/' ||
+				HTML.#code[HTML.#current] === '!'
+			) HTML.#handle_symbols();
 
-      // ' '
-      if(HTML.#code[HTML.#current] === ' ') HTML.#handleWhitespace();
+			// ' '
+			if(HTML.#code[HTML.#current] === ' ') HTML.#handle_whitespace();
 
-      // String
-      if(HTML.#code[HTML.#current] === '"' || HTML.#code[HTML.#current] === "'") HTML.#handleString();
+			// String
+			if(HTML.#code[HTML.#current] === '"' || HTML.#code[HTML.#current] === "'") HTML.#handle_string();
 
-      // '>'
-      if(HTML.#code.substring(HTML.#current, HTML.#current+4) === "&gt;"){
-        HTML.#handleSymbols('>', 4);
-        break;
-      }
+			// '>'
+			if(HTML.#code.substring(HTML.#current, HTML.#current+4) === "&gt;"){
+				HTML.#handle_symbols('>', 4);
+				break;
+			}
 
-      // Anything else = attributes
-      HTML.#tokens.push({type: 'attribute', value: HTML.#code[HTML.#current]});
-    }
-  }
+			// Anything else = attributes
+			HTML.#tokens.push({type: 'attribute', value: HTML.#code[HTML.#current]});
+		}
+	}
 
-  // Symbols inside element: meaning < / ! - =  "" >
-  static #handleSymbols(symbol = null, length = 1){
-    HTML.#tokens.push({type: 'symbol', value: symbol || HTML.#code[HTML.#current]});
-    HTML.#current += length;
-  }
+	// Symbols inside element: meaning < / ! - =  "" >
+	static #handle_symbols(symbol = null, length = 1){
+		HTML.#tokens.push({type: 'symbol', value: symbol || HTML.#code[HTML.#current]});
+		HTML.#current += length;
+	}
 
-  static #handleString(){
-    let string = `"`;
-    let quote = HTML.#code[HTML.#current];
+	static #handle_string(){
+		let string = `"`;
+		let quote = HTML.#code[HTML.#current];
 
-    while(HTML.#code[++HTML.#current] !== quote && HTML.#current < HTML.#code.length) string += HTML.#code[HTML.#current];
+		while(HTML.#code[++HTML.#current] !== quote && HTML.#current < HTML.#code.length) string += HTML.#code[HTML.#current];
 
-    string += `"`;
+		string += `"`;
 
-    HTML.#tokens.push({type: 'string', value: string});
+		HTML.#tokens.push({type: 'string', value: string});
 
-    HTML.#current++;
-  }
+		HTML.#current++;
+	}
 };
--- source/languages/JavaScript.js
+++ source/languages/JavaScript.js
@@ -1,171 +1,171 @@
 export default class JavaScript{
-  static #tokens = null;
+	static #tokens = null;
 
-  static handle(code){
-    JavaScript.#tokenize(code);
-    return JavaScript.#renderHighlightedCode();
-  }
+	static handle(code){
+		JavaScript.#tokenize(code);
+		return JavaScript.#render_highlighted_code();
+	}
 
-  static #tokenize(code){
-    const keywords = ['if', 'else', 'while', 'for', 'function', 'var', 'let', 'const', 'return'];
-    const operators = ['+', '-', '*', '/', '=', '==', '!=', '>', '<', '>=', '<=', '(', ')', '{', '}'];
+	static #tokenize(code){
+		const keywords = ['if', 'else', 'while', 'for', 'function', 'var', 'let', 'const', 'return'];
+		const operators = ['+', '-', '*', '/', '=', '==', '!=', '>', '<', '>=', '<=', '(', ')', '{', '}'];
 
-    const tokens = [];
-    let current = 0;
+		const tokens = [];
+		let current = 0;
 
-    while(current < code.length){
-      let char = code[current];
+		while(current < code.length){
+			let char = code[current];
 
-      // Whitespace
-      if([' ', '\s', '\t'].includes(char)){
-        tokens.push({ type: 'whitespace', value: ' ' });
+			// Whitespace
+			if([' ', '\s', '\t'].includes(char)){
+				tokens.push({ type: 'whitespace', value: ' ' });
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Newline
-      if(char == '\n'){
-        tokens.push({type: 'newline', value: '\n'});
+			// Newline
+			if(char == '\n'){
+				tokens.push({type: 'newline', value: '\n'});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Comments
-      if(char === '/' && code[current + 1] === '/'){
-        let comment = '/';
+			// Comments
+			if(char === '/' && code[current + 1] === '/'){
+				let comment = '/';
 
-        while(code[++current] !== '\n' && current < code.length) comment += code[current];
+				while(code[++current] !== '\n' && current < code.length) comment += code[current];
 
-        comment += '\n'
+				comment += '\n'
 
-        tokens.push({type: 'comment', value: comment});
+				tokens.push({type: 'comment', value: comment});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Dot
-      if(char === '.'){
-        tokens.push({type: 'dot', value: '.'});
+			// Dot
+			if(char === '.'){
+				tokens.push({type: 'dot', value: '.'});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Semicolon
-      if(char === ';'){
-        tokens.push({type: 'semicolon', value: ';'});
+			// Semicolon
+			if(char === ';'){
+				tokens.push({type: 'semicolon', value: ';'});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Operators
-      if(operators.includes(char)){
-        let operator = char;
+			// Operators
+			if(operators.includes(char)){
+				let operator = char;
 
-        while(operators.includes(operator + code[current + 1])) operator += code[++current];
+				while(operators.includes(operator + code[current + 1])) operator += code[++current];
 
-        tokens.push({type: 'operator', value: operator});
+				tokens.push({type: 'operator', value: operator});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Keywords and Identifiers
-      if(/[a-zA-Z_]/.test(char)){
-        let identifier = char;
+			// Keywords and Identifiers
+			if(/[a-zA-Z_]/.test(char)){
+				let identifier = char;
 
-        while(/[a-zA-Z0-9_]/.test(code[current + 1])) identifier += code[++current];
+				while(/[a-zA-Z0-9_]/.test(code[current + 1])) identifier += code[++current];
 
-        if(keywords.includes(identifier)) tokens.push({type: 'keyword', value: identifier});
-        else tokens.push({type: 'identifier', value: identifier});
+				if(keywords.includes(identifier)) tokens.push({type: 'keyword', value: identifier});
+				else tokens.push({type: 'identifier', value: identifier});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Numbers
-      if(/[0-9]/.test(char)){
-        let number = char;
+			// Numbers
+			if(/[0-9]/.test(char)){
+				let number = char;
 
-        while(/[0-9.]/.test(code[current + 1])) number += code[++current];
+				while(/[0-9.]/.test(code[current + 1])) number += code[++current];
 
-        tokens.push({type: 'number', value: parseFloat(number)});
+				tokens.push({type: 'number', value: parseFloat(number)});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // String
-      if(char === '"' || char === "'"){
-        let string = `"`;
-        let quote = char;
+			// String
+			if(char === '"' || char === "'"){
+				let string = `"`;
+				let quote = char;
 
-        while(code[++current] !== quote) string += code[current];
+				while(code[++current] !== quote) string += code[current];
 
-        string += `"`;
+				string += `"`;
 
-        tokens.push({type: 'string', value: string});
+				tokens.push({type: 'string', value: string});
 
-        current++;
-        continue;
-      }
+				current++;
+				continue;
+			}
 
-      // Handle unrecognized characters
-      tokens.push({type: 'unknown', value: char});
+			// Handle unrecognized characters
+			tokens.push({type: 'unknown', value: char});
 
-      current++;
-    }
+			current++;
+		}
 
-    JavaScript.#tokens = tokens;
-  }
+		JavaScript.#tokens = tokens;
+	}
 
-  static #renderHighlightedCode(){
-    let html = '';
+	static #render_highlighted_code(){
+		let HTML = '';
 
-    for(const token of JavaScript.#tokens){
-      let tokenHtml = '';
+		for(const token of JavaScript.#tokens){
+			let token_HTML = '';
 
-      switch(token.type){
-        case 'comment':
-          tokenHtml = `<span style="color: gray;">${token.value}</span>`;
-          break;
+			switch(token.type){
+				case 'comment':
+					token_HTML = `<span style="color: gray;">${token.value}</span>`;
+					break;
 
-        case 'dot':
-        case 'semicolon':
-          tokenHtml = `<span style="color: white;">${token.value}</span>`;
-          break;
+				case 'dot':
+				case 'semicolon':
+					token_HTML = `<span style="color: white;">${token.value}</span>`;
+					break;
 
-        case 'operator':
-          tokenHtml = `<span style="color: hsla(205, 100%, 83%, 1);">${token.value}</span>`;
-          break;
+				case 'operator':
+					token_HTML = `<span style="color: hsla(205, 100%, 83%, 1);">${token.value}</span>`;
+					break;
 
-        case 'keyword':
-          tokenHtml = `<span style="color: hsla(286, 38%, 58%, 1);">${token.value}</span>`;
-          break;
+				case 'keyword':
+					token_HTML = `<span style="color: hsla(286, 38%, 58%, 1);">${token.value}</span>`;
+						break;
 
-        case 'identifier':
-          tokenHtml = `<span style="color: hsla(205, 100%, 48%, 1);">${token.value}</span>`;
-          break;
+				case 'identifier':
+					token_HTML = `<span style="color: hsla(205, 100%, 48%, 1);">${token.value}</span>`;
+					break;
 
-        case 'number':
-          tokenHtml = `<span style="color: hsla(33, 38%, 58%, 1);">${token.value}</span>`;
-          break;
+				case 'number':
+					token_HTML = `<span style="color: hsla(33, 38%, 58%, 1);">${token.value}</span>`;
+					break;
 
-        case 'string':
-          tokenHtml = `<span style="color: hsla(124, 38%, 58%, 1);">${token.value}</span>`;
-          break;
+				case 'string':
+					token_HTML = `<span style="color: hsla(124, 38%, 58%, 1);">${token.value}</span>`;
+					break;
 
-        default:
-          tokenHtml = `<span>${token.value}</span>`;
-      }
+				default:
+					token_HTML = `<span>${token.value}</span>`;
+			}
 
-      html += tokenHtml;
-    }
+			HTML += token_HTML;
+		}
 
-    return html;
-  }
+		return HTML;
+	}
 };
